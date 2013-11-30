aa32a2e9a1305923405bb755d184f5a181392097
cent4ur <don.sasha@gmail.com>
2013-11-30 19:09:41 +0100

    Added Line.findAll named query.

--- alwara-webapp/src/main/java/hu/cent4ur/alwara/model/Line.java
+++ alwara-webapp/src/main/java/hu/cent4ur/alwara/model/Line.java
@@ -3,7 +3,10 @@
 // BSD-style license that can be found in the LICENSE file.
 package hu.cent4ur.alwara.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -12,8 +15,20 @@ import javax.validation.constraints.NotNull;
  * @author Sandor Balazs
  */
 @Entity
+@NamedQueries({
+        @NamedQuery(
+            name = "Line.findAll",
+            query = "SELECT l FROM Line l") })
 public class Line extends EntityBase {
 
     @NotNull
+    @Column(unique = true)
     private String name;
+
+    public Line() {
+    }
+
+    public Line(String name) {
+        this.name = name;
+    }
 }
