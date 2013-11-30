aea84db81fe2c70e7ca00ad8f2a78df61493a4e8
cent4ur <don.sasha@gmail.com>
2013-12-01 00:18:27 +0100

    Added row selection and header information.

--- alwara-webapp/src/main/java/hu/cent4ur/alwara/web/LineBean.java
+++ alwara-webapp/src/main/java/hu/cent4ur/alwara/web/LineBean.java
@@ -1,3 +1,6 @@
+// Copyright (c) 2013, the Alwara project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 package hu.cent4ur.alwara.web;
 
 import hu.cent4ur.alwara.model.Line;
@@ -8,12 +11,16 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+
+import org.primefaces.event.SelectEvent;
 
 /**
  * Managing lines.
@@ -28,19 +35,40 @@ public class LineBean implements Serializable {
     private static final Logger logger = Logger.getLogger(LineBean.class.getName());
     private final EntityManager entityManager;
     private String lineName;
+    private Line selectedLine;
+    private LineDataModel lineDataModel;
 
     public LineBean() {
+        logger.info("LineBean() - Initialization.");
         EntityManagerFactory emf = Persistence
                 .createEntityManagerFactory("AlwaraPersistenceUnit");
         entityManager = emf.createEntityManager();
+        updateLineDataModel();
     }
 
-    public void saveLine(ActionEvent evt) {
-        logger.info("saveLine() - Save line information.");
-        entityManager.getTransaction().begin();
-        Line line = new Line(lineName);
-        entityManager.persist(line);
-        entityManager.getTransaction().commit();
+    public String getLineName() {
+        return lineName;
+    }
+
+    public void setLineName(String lineName) {
+        this.lineName = lineName;
+    }
+
+    public Line getSelectedLine() {
+        return selectedLine;
+    }
+
+    public void setSelectedLine(Line selectedLine) {
+        this.selectedLine = selectedLine;
+    }
+
+    public LineDataModel getLineDataModel() {
+        return lineDataModel;
+    }
+
+    public void updateLineDataModel() {
+        logger.info("updateLineDataModel() - Updating lines.");
+        lineDataModel = new LineDataModel(getLines());
     }
 
     public List<Line> getLines() {
@@ -50,10 +78,25 @@ public class LineBean implements Serializable {
         return query.getResultList();
     }
 
+    public void saveLine(ActionEvent event) {
+        logger.info("saveLine() - Save line information.");
+        entityManager.getTransaction().begin();
+        Line line = new Line(lineName);
+        entityManager.persist(line);
+        entityManager.getTransaction().commit();
+        updateLineDataModel();
+    }
+
     public List<Link> getLinks() {
         logger.info("getLinks() - Query all links.");
         TypedQuery<Link> query = entityManager.createNamedQuery("Link.findAll",
                 Link.class);
         return query.getResultList();
+    }
+
+    public void onRowSelect(SelectEvent event) {
+        FacesMessage msg = new FacesMessage("Line Selected",
+                ((Line) event.getObject()).getName());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 }
