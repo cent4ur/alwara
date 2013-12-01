1b5717fd1f4d8a64a3726ae32f82785af0dbb311
cent4ur <don.sasha@gmail.com>
2013-12-01 10:03:53 +0100

    Link and Line merge.

--- alwara-webapp/src/main/java/hu/cent4ur/alwara/model/Line.java
+++ alwara-webapp/src/main/java/hu/cent4ur/alwara/model/Line.java
@@ -3,14 +3,19 @@
 // BSD-style license that can be found in the LICENSE file.
 package hu.cent4ur.alwara.model;
 
-import javax.persistence.Column;
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
 /**
- * A line contains links that connect stations in a pre-defined order.
+ * Connects stations in a pre-defined order.
  * 
  * @author Sandor Balazs
  */
@@ -19,29 +24,129 @@ import javax.validation.constraints.NotNull;
         @NamedQuery(
             name = "Line.findAll",
             query = "SELECT l FROM Line l") })
-public class Line extends EntityBase {
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "ROUTE_FK",
+        "ORDINAL" }))
+public class Line extends EntityBase implements Serializable {
+
+    private static final long serialVersionUID = 3041916241054853666L;
 
     @NotNull
-    @Column(unique = true)
-    private String name;
+    @ManyToOne
+    @JoinColumn(name = "ROUTE_FK")
+    private Route route;
+
+    // The n-th place in the line of stations.
+    @NotNull
+    private Integer ordinal;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "START_STATION_FK")
+    private Station startStation;
+
+    @ManyToOne
+    @JoinColumn(name = "STOP_STATION_FK")
+    private Station stopStation;
+
+    // Bus or train line.
+    private Boolean bus;
+    private Boolean train;
+
+    // The cost of traveling the line.
+    private Integer cost;
+
+    // The length of the line.
+    private Integer length;
+
+    // The time (in minutes) taken to travel the line.
+    private Integer time;
 
     public Line() {
     }
 
-    public Line(String name) {
-        this.name = name;
+    public Line(Route route, Integer ordinal, Station startStation,
+            Station stopStation, Boolean bus, Boolean train, Integer cost,
+            Integer length, Integer time) {
+        this.route = route;
+        this.ordinal = ordinal;
+        this.startStation = startStation;
+        this.stopStation = stopStation;
+        this.bus = bus;
+        this.train = train;
+        this.cost = cost;
+        this.length = length;
+        this.time = time;
     }
 
-    public String getName() {
-        return name;
+    public Route getRoute() {
+        return route;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setRoute(Route route) {
+        this.route = route;
     }
 
-    @Override
-    public String toString() {
-        return name;
+    public Integer getOrdinal() {
+        return ordinal;
+    }
+
+    public void setOrdinal(Integer ordinal) {
+        this.ordinal = ordinal;
+    }
+
+    public Station getStartStation() {
+        return startStation;
+    }
+
+    public void setStartStation(Station startStation) {
+        this.startStation = startStation;
+    }
+
+    public Station getStopStation() {
+        return stopStation;
+    }
+
+    public void setStopStation(Station stopStation) {
+        this.stopStation = stopStation;
+    }
+
+    public Boolean getBus() {
+        return bus;
+    }
+
+    public void setBus(Boolean bus) {
+        this.bus = bus;
+    }
+
+    public Boolean getTrain() {
+        return train;
+    }
+
+    public void setTrain(Boolean train) {
+        this.train = train;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public Integer getTime() {
+        return time;
+    }
+
+    public void setTime(Integer time) {
+        this.time = time;
     }
 }
