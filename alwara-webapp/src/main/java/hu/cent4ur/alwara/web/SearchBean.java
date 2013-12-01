12ddade5220d07b42b7327a9f2b7a43418af643e
cent4ur <don.sasha@gmail.com>
2013-12-01 14:13:06 +0100

    Initial version.

---
+++ alwara-webapp/src/main/java/hu/cent4ur/alwara/web/SearchBean.java
@@ -0,0 +1,61 @@
+// Copyright (c) 2013, the Alwara project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package hu.cent4ur.alwara.web;
+
+import hu.cent4ur.alwara.model.Station;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ * Managing searches.
+ * 
+ * @author Sandor Balazs
+ */
+@Named
+@SessionScoped
+public class SearchBean implements Serializable {
+
+    private static final long serialVersionUID = 7119305520329822712L;
+    private static final Logger logger = Logger.getLogger(SearchBean.class
+            .getName());
+    private Date startDate;
+    private Station startStation;
+    private Station endStation;
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Station getStartStation() {
+        return startStation;
+    }
+
+    public void setStartStation(Station startStation) {
+        this.startStation = startStation;
+    }
+
+    public Station getEndStation() {
+        return endStation;
+    }
+
+    public void setEndStation(Station endStation) {
+        this.endStation = endStation;
+    }
+
+    public SearchBean() {
+        logger.info("SearchBean() - Initialization.");
+    }
+
+    public void search() {
+    }
+}
