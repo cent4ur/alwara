b040714a3419493892b2b6d67b510ce67a6901c0
cent4ur <don.sasha@gmail.com>
2013-12-01 01:35:34 +0100

    Initial version.

---
+++ alwara-webapp/src/main/java/hu/cent4ur/alwara/web/LoginBean.java
@@ -0,0 +1,42 @@
+// Copyright (c) 2013, the Alwara project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package hu.cent4ur.alwara.web;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ * Managing login.
+ * 
+ * @author Sandor Balazs
+ */
+@Named
+@SessionScoped
+public class LoginBean {
+
+    private String username;
+    private String password;
+
+    public LoginBean() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void login() {
+    }
+}
